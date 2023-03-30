e3cb51ba215ed602e75158b9dc191e8c8d955dbd
mgngm <mgngm@amazon.com>
2023-03-30 12:04:53 +0200

    test

--- amplify/hooks/post-push.sh
+++ amplify/hooks/post-push.sh
@@ -2,12 +2,4 @@
 
 echo $AWS_JOB_ID
 
-if [ -z "$AWS_JOB_ID" ]; then
-  echo "Generating graphql map..."
-  npm run generate-graphql-map
-  echo "Graphql map generation complete..."
-  exit 0
-else
-  echo "Hooks disabled on CI"
-  exit 0
-fi
+echo "Hooks disabled on CI"
